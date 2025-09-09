5b12426107bbaaf10e4d851b22986a3dc7fd1427
Rey <rebekavictoriabs@gmail.com>
2025-09-09 20:45:45 -0300

    Adicionando o Front da aba Cadastro

--- frontend/autenticacao/src/app/cadastro/page.js
+++ frontend/autenticacao/src/app/cadastro/page.js
@@ -1,7 +1,33 @@
+"use client"
+import {useState} from "react"
 import React from 'react'
 
 export default function RegisterPage() {
+
+    const [userName, setUserName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
     return (
-        <div>Cadastro</div>
+        <div className="min-h-screen bg-gray-100 items-center flex flex-col justify-center">
+
+            <form>
+
+                <label>Nome de usuário</label>
+                <input type="text" value={userName} required onChange={(e)=>{setUserName(e.target.value)}}/>
+                <br/>
+
+                <label>E-mail</label>
+                <input type="text" value={email} required onChange={(e)=>{setEmail(e.target.value)}}/>
+                <br/>
+
+                <label>Senha</label>
+                <input type="text" value={password} required onChange={(e)=>{setPassword(e.target.value)}}/>
+                <br/>
+
+                <button type="submit">Cadastrar</button>
+            </form>
+
+        </div>
     )
 }
--- utils/auth.js
+++ utils/auth.js
@@ -1,11 +1,24 @@
-export const registerUser = async (params) => {
+import axios from "axios";
+
+const API_URL = "http://127.0.0.1:8000/api/usuarios/";
+
+export const registerUser = async (email, password, username) => {
+    try {
+        const response = await axios.post(`${API_URL}register`, {email, password, username},
+            {withCredentials: true}    
+        )
+        return response.data;      
+    }
+    catch (e) {
+        throw new Error("Cadastro não concluído.")
+    }
 
 }
 
-export const loginrUser = async (params) => {
+export const loginUser = async (email, password) => {
 
 }
 
-export const logoutrUser = async (params) => {
+export const logoutUser = async (params) => {
 
 }
