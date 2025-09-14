ec43216aee07ecaa236d6613bd3038ff8a481cd7
Rey <rebekavictoriabs@gmail.com>
2025-09-14 15:59:07 -0300

    Adicionando lógica de funcionamento para o login e o cadastro, recuperando dados do backend

--- frontend/projeto/src/app/cadastro/page.js
+++ frontend/projeto/src/app/cadastro/page.js
@@ -1,5 +1,6 @@
 "use client"
 import {useState} from "react"
+import { registerUser } from "../../../utils/auth";
 import React from 'react'
 
 export default function RegisterPage() {
@@ -8,24 +9,38 @@ export default function RegisterPage() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        if(userName==="" || password ==="" || email==="") {
+            return
+        }
+        try {
+            await registerUser(email, password, userName)
+            alert("Usuário registrado!")
+        }
+        catch (e) {
+            alert("Usuário não pode ser registrado")
+        }
+    }
+
     return (
         <div className="min-h-screen bg-gray-100 items-center flex flex-col justify-center">
 
-            <form className="bg-gray--600 p-8 flex flex-col ga">
+            <form onSubmit={handleSubmit} className="bg-gray-600 p-8 flex flex-col rounded-lg">
 
                 <label>Nome de usuário</label>
-                <input type="text" value={userName} required onChange={(e)=>{setUserName(e.target.value)}}/>
+                <input className="text-white-600" type="text" value={userName} required onChange={(e)=>{setUserName(e.target.value)}}/>
                 <br/>
 
                 <label>E-mail</label>
-                <input type="text" value={email} required onChange={(e)=>{setEmail(e.target.value)}}/>
+                <input className="text-white-600" type="text" value={email} required onChange={(e)=>{setEmail(e.target.value)}}/>
                 <br/>
 
                 <label>Senha</label>
-                <input type="text" value={password} required onChange={(e)=>{setPassword(e.target.value)}}/>
+                <input className="text-white-600" type="password" value={password} required onChange={(e)=>{setPassword(e.target.value)}}/>
                 <br/>
 
-                <button type="submit">Cadastrar</button>
+                <button className="bg-blue-400 p-1 rounded-sm" type="submit">Cadastrar</button>
             </form>
 
         </div>
--- frontend/projeto/src/app/login/page.js
+++ frontend/projeto/src/app/login/page.js
@@ -1,7 +1,40 @@
+"use client"
 import React from 'react'
+import {useState} from "react"
+import {loginUser} from "../../../utils/auth"
 
 export default function LoginPage() {
+    const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("")
+
+    const handleSubmit = async (e) => {
+            e.preventDefault()
+            if(password ==="" || email==="") {
+                return
+            }
+            try {
+                await loginUser(email, password)
+                alert("Login realizado com sucesso!")
+            }
+            catch (e) {
+                alert("Login não pode ser realizado.")
+            }
+        }
+
     return (
-        <div>Login</div>
+        <div  className="min-h-screen bg-gray-100 items-center flex flex-col justify-center">
+             <form onSubmit={handleSubmit} className="bg-gray-600 p-8 flex flex-col rounded-lg">
+
+                <label>E-mail</label>
+                <input className="text-white-600" type="text" value={email} required onChange={(e)=>{setEmail(e.target.value)}}/>
+                <br/>
+
+                <label>Senha</label>
+                <input className="text-white-600" type="password" value={password} required onChange={(e)=>{setPassword(e.target.value)}}/>
+                <br/>
+
+                <button className="bg-blue-400 p-1 rounded-sm" type="submit">Entrar</button>
+            </form>
+        </div>
     )
 }
