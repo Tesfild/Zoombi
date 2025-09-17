9bb10db764733c3032f115ade385e197bcff854f
Rey <rebekavictoriabs@gmail.com>
2025-09-17 00:16:27 -0300

    Alteração no tempo de vida de Token

--- backend/authUsuario/settings.py
+++ backend/authUsuario/settings.py
@@ -106,7 +106,7 @@ REST_FRAMEWORK = {
 
 # Configurações dos tokens
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=0.2),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
