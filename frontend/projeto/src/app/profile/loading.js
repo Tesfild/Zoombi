f9f928eff1a0c63164fe632fa7893ec817d5a8d6
Rey <rebekavictoriabs@gmail.com>
2025-09-21 22:23:31 -0300

    Correção loading

--- frontend/projeto/src/app/profile/loading.js
+++ frontend/projeto/src/app/profile/loading.js
@@ -1,7 +1,7 @@
 export default function Loading() {
     return (
         <div className="flex h-screen items-center justify-center flex-col">
-            <h1 className="text-xl font-semibold">Carregando perfil...</h1>
+            <h1 className="text-xl font-semibold">Carregando...</h1>
             <div className="animate-spin mt-4 h-10 w-10 rounded-full border-4 border-gray-300 border-t-gray-900"></div>
         </div>
     )
